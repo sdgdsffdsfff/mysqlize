16a3d352d852e66b5ed4393fede116087e586714
Eddie Ramirez <djedi.r@gmail.com>
2015-07-15 02:42:17 -0500

    Added actions buttons

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,6 @@
 //alert(window.location.pathname);
 $(document).ready(function(){
-  optionName = window.location.pathname
-  var setOptionTitle = function(){
-    $('.option-title').text(optionName);
-  };
-  setOptionTitle();
-  
-  $.getJSON('/options/xtrabackup/2.2.3', function(data){
-      data.forEach(function(element, index, array){
-        $('.panel-body-options').append('<span style="display:block">' + element.name + element.longOption + element.description + '</span>');  
-      });
+  $('#button-privileges-xtrabackup').click(function() {
+    $('#privileges-xtrabackup').show('fast');
   });
 });
